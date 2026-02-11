3b2136758766bf3e569060630857a86b7a2924da
KingGray0-0 <satyamkumam5@gmail.com>
2026-02-11 14:41:13 +0530

    Time: 0 ms (100%), Space: 522.2 MB (48.24%) - LeetHub

--- 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
     int longestBalanced(vector<int>& nums) {
-        int n = nums.size();
-        int m = 0;
+    int n = nums.size();
+        int maxLen = 0;
 
         for (int i = 0; i < n; i++) {
-            unordered_set<int> e, o;
+            unordered_map<int,int> freq;
+            int evenCount = 0;
+            int oddCount = 0;
 
             for (int j = i; j < n; j++) {
-                if (nums[j] % 2 == 0)
-                    e.insert(nums[j]);
-                else
-                    o.insert(nums[j]);
+                if (freq[nums[j]] == 0) {
+                    if (nums[j] % 2 == 0)
+                        evenCount++;
+                    else
+                        oddCount++;
+                }
 
-                if (e.size() == o.size()) {
-                    m = max(m, j - i + 1);
+                freq[nums[j]]++;
+
+                if (evenCount == oddCount) {
+                    maxLen = max(maxLen, j - i + 1);
                 }
             }
         }
-        return m;
+
+        return maxLen;
     }
 };
 auto __fast_io_atexit = []() { ios::sync_with_stdio(false); cin.tie(nullptr); std::atexit([]() { ofstream("display_runtime.txt") << "0"; }); return 0; }();
