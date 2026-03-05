fbb246dc79b0d7771d0baa2b29e122f85336d001
KingGray0-0 <satyamkumam5@gmail.com>
2026-03-05 17:41:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
     int minOperations(string s) {
-         int n = s.size();
-        
-        string s1 = s;
-        string s2 = s;
+        int n = s.size();
+        int c1=0;
+        int c2=0;
 
-        int c1 = 0, c2 = 0;
+        for(int i=0 ;i<n;i++){
+            if(i%2==0){
+                if(s[i]!='0') c1++;
+                if(s[i]!='1') c2++;
 
-        for(int i = 1; i < n; i++){
-            if(s1[i-1] == s1[i]){
-                if(s1[i] == '0')
-                    s1[i] = '1';
-                else
-                    s1[i] = '0';
-                c1++;
+            }else{
+                if(s[i]!='1') c1++;
+                if(s[i]!='0') c2++;
             }
         }
-
-        for(int i = n-2; i >= 0; i--){
-            if(s2[i] == s2[i+1]){
-                if(s2[i] == '0')
-                    s2[i] = '1';
-                else
-                    s2[i] = '0';
-                c2++;
-            }
-        }
-
-        return min(c1, c2);
+        if(c1<=c2) return c1;
+        else return c2; 
     }
 };
